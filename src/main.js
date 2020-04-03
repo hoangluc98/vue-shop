b2b669f75421a8b1326df948a3e6e63295ceea53
hoangluc98 <luclego98@gmail.com>
2020-04-03 16:12:54 +0700

    [1.0.0] Check login, register form.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import jQuery from 'jquery';
+import Vuelidate from "vuelidate";
+Vue.use(Vuelidate);
 
 window.$ = window.jQuery = jQuery;
 
